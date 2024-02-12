53240a2b0bbc7d10070e93663cafee5b480f5717
JoshuaMaitland <joshuamaitland1999@outlook.com>
2024-02-12 21:09:06 +0000

    Added AOpen MX64
    https://theretroweb.com/motherboards/s/aopen-mx64

--- src/include/86box/machine.h
+++ src/include/86box/machine.h
@@ -775,6 +775,7 @@ extern int machine_at_p6sba_init(const machine_t *);
 extern int machine_at_ficka6130_init(const machine_t *);
 extern int machine_at_p3v133_init(const machine_t *);
 extern int machine_at_p3v4x_init(const machine_t *);
+extern int machine_at_mx64_init(const machine_t *);
 extern int machine_at_gt694va_init(const machine_t *);
 
 extern int machine_at_vei8_init(const machine_t *);
